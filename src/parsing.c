e7d705721c5342230ecb881500b4da6014fb1a1c
Alexis LOUBIERE <aloubier@e3r7p21.clusters.42paris.fr>
2023-04-25 18:45:43 +0200

    Added duplicate parsing

--- src/parsing.c
+++ src/parsing.c
@@ -6,8 +6,32 @@
 /*   By: aloubier <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/25 17:18:48 by aloubier          #+#    #+#             */
-/*   Updated: 2023/04/25 17:19:26 by aloubier         ###   ########.fr       */
+/*   Updated: 2023/04/25 18:45:20 by aloubier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_pushswap.h"
+
+int	is_duplicate(char **value)
+{
+	char *tmp;
+	int	i;
+	int	current;
+
+	i = 0;
+	tmp = value[i];
+	current = 0;
+	while (value[current])
+	{
+		i = current;
+		tmp = value[current];
+		while (!value[i])
+		{
+			if (atoi(tmp) == atoi(value[i]))
+				return (1);
+			i++;
+		}
+		current++;
+	}
+	return (0);
+}
