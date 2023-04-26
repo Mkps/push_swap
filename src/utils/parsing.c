e71d4bbd521ea7afcb188ef457b66c6328915514
Alexis LOUBIERE <aloubier@e3r5p20.clusters.42paris.fr>
2023-04-26 15:43:37 +0200

    Added parsing for invalid characters

--- src/utils/parsing.c
+++ src/utils/parsing.c
@@ -6,13 +6,37 @@
 /*   By: aloubier <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/25 17:18:48 by aloubier          #+#    #+#             */
-/*   Updated: 2023/04/25 19:24:31 by aloubier         ###   ########.fr       */
+/*   Updated: 2023/04/26 15:43:11 by aloubier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_push_swap.h"
 
-int	parse_error(char **value)
+//Parse the provided string array for invalid characters
+
+int	parse_invalid(char **value)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (value[i] != NULL)
+	{
+		j = 0;
+		if (value[i][j] == '-' || (value[i][j] >= '0' && value[i][j] <= '9'))
+			j++;
+		while (value[i][j] != NULL)
+		{
+			if ( value[i][j] < '0'|| value[i][j] > '9')
+				return (1);
+			j++;
+		}
+		i++;
+	}
+}
+
+
+int	parse_duplicate(char **value)
 {
 	char *tmp;
 	int	i;
