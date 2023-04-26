0df94ad2401b44c80bfbe5ef8b8f2a88acf82567
Alexis LOUBIERE <aloubier@e3r5p20.clusters.42paris.fr>
2023-04-26 16:40:11 +0200

    Added some libft functions

---
+++ src/utils/libft/libft.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aloubier <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/26 16:33:27 by aloubier          #+#    #+#             */
+/*   Updated: 2023/04/26 16:34:49 by aloubier         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+# include <stddef.h>
+# include <stdlib.h>
+# include <limits.h>
+# include <unistd.h>
+# include <stdint.h>
+
+int	    ft_strcmp(char *s1, char *s2);
+char	**ft_split(char const *str, char c);
+int	    ft_lstsize(t_list *lst);
+t_list	*ft_lstlast(t_list *lst);
+void	ft_lstdelone(t_list *lst, void (*del)(void*));
+void	ft_lstclear(t_list **lst, void (*del)(void*));
+void	ft_lstadd_front(t_list **lst, t_list *new);
+void	ft_lstadd_back(t_list **lst, t_list *new);
+int	    ft_atoi(const char *str);
+
+#endif
